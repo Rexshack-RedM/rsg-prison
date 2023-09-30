e171ec278f2b4a77ed895fdf8c071afcb3b056c0
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:38:33 +0100

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,14 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
+author 'RexShack#3041'
 description 'rsg-prison'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
 client_scripts {
     '@PolyZone/client.lua',
@@ -15,11 +22,6 @@ client_scripts {
 
 server_scripts {
     'server/server.lua'
-}
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'config.lua'
 }
 
 dependencies {
